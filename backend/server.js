1c6207fe290550817342fd8a7af5078b7eb79063
LatroncheQuentin <quentin.latronche@limayrac.fr>
2026-05-02 15:39:18 +0200

    fix bug create

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000",
-      "https://bmc-frontend-mocha.vercel.app", // ← votre vraie URL Vercel
+      "https://game-gourmands.vercel.app", // ← votre vraie URL Vercel
     ],
     methods: ["GET", "POST"],
   },
